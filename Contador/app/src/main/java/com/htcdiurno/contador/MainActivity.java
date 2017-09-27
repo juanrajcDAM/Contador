60d18e97ff1ca249c1cf81f9be2469acc557e57c
Unknown <juanrajc@hotmail.com>
2017-09-27 20:17:25 +0200

    Creo la función de contador.

--- Contador/app/src/main/java/com/htcdiurno/contador/MainActivity.java
+++ Contador/app/src/main/java/com/htcdiurno/contador/MainActivity.java
@@ -2,12 +2,26 @@ package com.htcdiurno.contador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double contador = 1;
+    private TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById (R.id.text);
+        texto.setText (String.valueOf (contador));
+    }
+
+    // duplica el valor del contador y lo guarda en la etiqueta texto
+    public void duplica (View view) {
+        contador = contador * 2;
+        texto.setText(String.valueOf(contador));
     }
 }
